e29595f1e8328c9b9bbb2eb8748e9a24c11a96bc
martintaylor1635 <martintaylor1635@gmail.com>
2024-07-08 20:56:56 +0100

    Refactored Mapper.java - removed unnecessary comment

--- src/main/java/com/visitscotland/wishlistapi/utility/Mapper.java
+++ src/main/java/com/visitscotland/wishlistapi/utility/Mapper.java
@@ -1,11 +1,5 @@
 package com.visitscotland.wishlistapi.utility;
 
-/**
- * A simple functional interface for mapping between
- * two objects of different types.
- * @param <I> The INPUT type.
- * @param <O> The OUTPUT type.
- */
 @FunctionalInterface
 public interface Mapper<I, O> {
     O map(I input);
