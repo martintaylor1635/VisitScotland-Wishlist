d6fe9b9a06561975690fdfcbffaec14c07ebe07f
martintaylor1635 <martintaylor1635@gmail.com>
2024-07-08 20:56:34 +0100

    Refactored ItemAlreadyExistsException.java to display item title

--- src/main/java/com/visitscotland/wishlistapi/exception/ItemAlreadyExistsException.java
+++ src/main/java/com/visitscotland/wishlistapi/exception/ItemAlreadyExistsException.java
@@ -3,9 +3,9 @@ package com.visitscotland.wishlistapi.exception;
 import com.visitscotland.wishlistapi.exception.base.AlreadyExistsException;
 
 public class ItemAlreadyExistsException extends AlreadyExistsException {
-    private static final String EXCEPTION_MESSAGE = "The item with ID %s already exists";
+    private static final String EXCEPTION_MESSAGE = "The item with title '%s' already exists";
 
-    public ItemAlreadyExistsException(String itemId) {
-        super(EXCEPTION_MESSAGE.formatted(itemId));
+    public ItemAlreadyExistsException(String title) {
+        super(EXCEPTION_MESSAGE.formatted(title));
     }
 }
