4873e4d880387afe9a4b7011256d304abf5447e5
martintaylor1635 <martintaylor1635@gmail.com>
2024-07-08 20:59:49 +0100

    Added logic to Wishlist.java to check if an item exists within the collection of items, other refactors

--- src/main/java/com/visitscotland/wishlistapi/domain/Wishlist.java
+++ src/main/java/com/visitscotland/wishlistapi/domain/Wishlist.java
@@ -6,25 +6,19 @@ import java.util.Set;
 
 public class Wishlist {
     private final User user;
-    private Set<Item> items;
+    private final Set<Item> items;
 
     public Wishlist(User user) {
         this.user = Objects.requireNonNull(user);
         this.items = new HashSet<>();
     }
 
-    // Getters
     public User getUser() {
         return user;
     }
 
     public Set<Item> getItems() {
         return items;
-    }
-
-    // Setters
-    public void setItems(Set<Item> items) {
-        this.items = items;
     }
 
     public void addItem(Item item) {
@@ -35,5 +29,9 @@ public class Wishlist {
 
     public boolean removeItemByItemId(String itemId) {
         return items.removeIf(item -> item.getId().equals(itemId));
+    }
+
+    public boolean itemExists(Item item) {
+        return items.contains(item);
     }
 }
