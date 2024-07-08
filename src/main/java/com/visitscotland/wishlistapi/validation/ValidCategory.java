41f7f2b718366d2643d8f2e338ff9d77f992892c
martintaylor1635 <martintaylor1635@gmail.com>
2024-07-08 20:58:27 +0100

    Added functionality to allow the ValidCategory.java annotation on field level, removed method level access

--- src/main/java/com/visitscotland/wishlistapi/validation/ValidCategory.java
+++ src/main/java/com/visitscotland/wishlistapi/validation/ValidCategory.java
@@ -7,13 +7,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Target({ METHOD, PARAMETER })
 @Retention(RUNTIME)
+@Target({ PARAMETER, FIELD })
 @Constraint(validatedBy = CategoryValidator.class)
 public @interface ValidCategory {
     String message() default
