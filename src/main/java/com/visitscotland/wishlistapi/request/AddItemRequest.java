233badf38ac24c390e62525da109c9d62d3b975b
martintaylor1635 <martintaylor1635@gmail.com>
2024-07-08 20:55:45 +0100

    Added further validation to AddItemRequest.java

--- src/main/java/com/visitscotland/wishlistapi/request/AddItemRequest.java
+++ src/main/java/com/visitscotland/wishlistapi/request/AddItemRequest.java
@@ -1,17 +1,21 @@
 package com.visitscotland.wishlistapi.request;
 
 import com.visitscotland.wishlistapi.validation.ValidCategory;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.validation.annotation.Validated;
 
-import java.net.URI;
 import java.time.ZonedDateTime;
 import java.util.Map;
 
+@Validated
 public record AddItemRequest(
-    String title,
-    String description,
-    @ValidCategory
-    String category,
-    URI image,
-    ZonedDateTime eventDateTime,
-    Map<String, Object> metadata
+    @NotBlank String title,
+    @NotBlank String description,
+    @ValidCategory String category,
+    @NotBlank @URL String image,
+    @NotNull @FutureOrPresent ZonedDateTime eventDateTime,
+    Map<String, String> metadata
 ) { }
